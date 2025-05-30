7a87616209ff6cab80dd8627f2f0a5dd9c626212
NegaoDoBk <168202993+NegaoDoBk@users.noreply.github.com>
2025-05-30 11:31:07 -0300

    sckmaS

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+let count = 0;
+
+function updateDisplay() {
+  document.getElementById('count').textContent = count;
+}
+
+function increment() {
+  count++;
+  updateDisplay();
+}
+
+function decrement() {
+  count--;
+  updateDisplay();
+}
+
+function reset() {
+  count = 0;
+  updateDisplay();
+}
 
